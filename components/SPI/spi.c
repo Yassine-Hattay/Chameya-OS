ecc09cd5652556d5c02c24f894d721f1fa9a55f4
Yassine Hattay <hattayyassine519@gmail.com>
2025-03-25 18:42:07 +0100

    implemented mode 2 ( representing a pair of (CPOL, CPHA) configuration)

--- components/SPI/spi.c
+++ components/SPI/spi.c
@@ -12,7 +12,7 @@
 // Full-duplex SPI: Send and receive at the same time
 uint8_t spi_bit_bang_mode_0(uint8_t data_to_send) {
 	uint8_t received = 0;
-
+	gpio_set_level(SCK, 0);
 	// Pull CS low to start communication
 	gpio_set_level(SS, 0);
 	ets_delay_us(10);
@@ -42,7 +42,7 @@ uint8_t spi_bit_bang_mode_0(uint8_t data_to_send) {
 // Full-duplex SPI: Send and receive at the same time
 uint8_t spi_bit_bang_mode_1(uint8_t data_to_send) {
 	uint8_t received = 0;
-
+	gpio_set_level(SCK, 0);
 	// Pull CS low to start communication
 	gpio_set_level(SS, 0);
 	ets_delay_us(10);
@@ -64,12 +64,40 @@ uint8_t spi_bit_bang_mode_1(uint8_t data_to_send) {
 	// Return received data
 	return received;
 }
+
+
+
+// Full-duplex SPI: Send and receive at the same time
+uint8_t spi_bit_bang_mode_2(uint8_t data_to_send) {
+	uint8_t received = 0;
+	gpio_set_level(SCK, 1);
+	// Pull CS low to start communication
+	gpio_set_level(SS, 0);
+	ets_delay_us(10);
+
+	for (int i = 7; i >= 0; i--) {
+		// Set MOSI
+		gpio_set_level(MOSI, (data_to_send >> i) & 1);
+		ets_delay_us(10);
+		gpio_set_level(SCK, 0);
+		ets_delay_us(10);
+		received |= (gpio_get_level(MISO) << i);
+		gpio_set_level(SCK, 1);
+
+	}
+
+	// Pull CS high to end communication
+	gpio_set_level(SS, 1);
+
+	// Return received data
+	return received;
+}
 void spi_task(void *pvParameter) {
 	uint8_t data_to_send = 0xAA;  // Example data
 	uint8_t received_data = 0;
 
 	while (1) {
-		received_data = spi_bit_bang_mode_1(data_to_send);
+		received_data = spi_bit_bang_mode_2(data_to_send);
 		printf("Received: 0x%02X\n", received_data);
 
 		vTaskDelay(pdMS_TO_TICKS(1000));
@@ -90,8 +118,8 @@ void user_init(void) {
 			GPIO_PULLDOWN_DISABLE, .intr_type = GPIO_INTR_DISABLE };
 	gpio_config(&io_conf_slave);
 	// Default pin states
+
 	gpio_set_level(MOSI, 0);
-	gpio_set_level(SCK, 0);
 	gpio_set_level(SS, 1);
 
 	xTaskCreate(spi_task, "spi_task", 1024, NULL, 1, NULL);
